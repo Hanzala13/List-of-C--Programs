213ec7941f9817828d380c943b2bc9eb85fa4e92
Hanzala13 <azmihanzala123456789@gmail.com>
2024-12-14 23:30:56 +0530

    Area of square

---
+++ Area.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  double sideLength = 8.7;
+
+  double area = sideLength * sideLength;
+
+  cout << "Area of Square is : " << area;
+
+  return 0;
+}
