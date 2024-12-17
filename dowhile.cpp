59e689ad8019e38f3c2eb63fafe1e6b710d99c85
Hanzala13 <azmihanzala123456789@gmail.com>
2024-12-17 22:47:31 +0530

    While loop

---
+++ dowhile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num = 1;
+	do {
+		cout << "count : " << num << endl;
+		num++;
+		
+	}
+	while(num % 7 !=0);
+		cout << "Lucky 7!" <<endl;
+	
+}
---
+++ while.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int number = 5;
+	while( number >= 1){
+		cout << "Number is : " << number << endl;
+		number--;
+	}
+}
