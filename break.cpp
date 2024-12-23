9111490bd9c3601b35c1342f7d5e5c2fd3128c24
Hanzala13 <azmihanzala123456789@gmail.com>
2024-12-23 23:07:01 +0530

    Break Keyword

---
+++ break.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+ 
+    for (int i = 1; i < 6; ++i) {
+        cout << i << endl;
+        break;
+    }
+ 
+    cout << "This is outside the loop";
+ 
+    return 0;
+}
+
