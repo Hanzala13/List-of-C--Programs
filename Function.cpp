7e565e936d38c30468cd73959dcbdbcb178c9650
Hanzala13 <azmihanzala123456789@gmail.com>
2024-12-30 23:15:52 +0530

    Function in C++

---
+++ Function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+// function definition
+void MultiplyNumbers() {
+
+    int number1 = 8;
+    int number2 = 6;
+    int result = number1 * number2;
+        
+    cout << "Product of " << number1 <<" and "<< number2 << " is " << result << endl;
+}
+ 
+int main() {
+ 
+    // function call
+    MultiplyNumbers();
+ 
+    return 0;
+}
