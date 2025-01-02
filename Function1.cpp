b35848eb4109637773669f449de797c3f4c899a5
Hanzala13 <azmihanzala123456789@gmail.com>
2025-01-02 22:27:12 +0530

    Function example

---
+++ Function1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void BirthdayGreeting(){
+	for(int i=0; i<6;i++){
+		cout << "Happy Birthday" << endl;
+	}
+	
+}
+int main(){
+	BirthdayGreeting();
+	
+	return 0;
+}
