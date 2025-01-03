3da09bc28c02aaaf0864a809db1b4bc84e5bce08
Hanzala13 <azmihanzala123456789@gmail.com>
2025-01-03 16:44:54 +0530

    Function example 2

---
+++ Function2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void multiply(){
+	int num1 = 4;
+	int num2 = 5;
+	int result = num1 * num2;
+	cout << "The multiplication of " << num1 << " and " << num2 << " is " << result ;
+}
+
+int main(){
+	
+	multiply();
+	
+	return 0;
+}
