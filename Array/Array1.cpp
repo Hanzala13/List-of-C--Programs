abfba0d853eec32a8b6456792a3fc332e47e1ae8
Hanzala13 <azmihanzala123456789@gmail.com>
2025-01-16 23:09:52 +0530

    Array

---
+++ Array/Array1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    // create an array named arr of size 5
+    int arr[5] = {1, 2, 3, 4, 5};
+ 
+    // access first element
+    cout << arr[0] << endl;
+ 
+    // access second element
+    cout << arr[1] << endl;
+ 
+    // access third element
+    cout << arr[2] << endl;
+ 
+    // access fourth element
+    cout << arr[3] << endl;
+ 
+    // access fifth element
+    cout << arr[4];
+  
+    return 0;
+    }
+
