c5072c347053bfc401e91bca430fd18d0e12d6c0
Hanzala13 <azmihanzala123456789@gmail.com>
2025-01-17 18:24:28 +0530

    Array

---
+++ Array/Array2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int num[7] = {1, 34, 56, 78, 96, 54, 65};
+	
+	for(int index =0; index < 7; index++){
+		cout << num[index] << endl;
+	}
+	return 0;
+}
